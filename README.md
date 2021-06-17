38f969f94b30c231e5d36e31ddc4a50a84d5adc6
Christopher Caldarella <christopher.caldarella@gmail.com>
2021-06-16 22:35:39 -0400

    first!

---
+++ README.md
@@ -0,0 +1,54 @@
+# Hotdog Not Hotdog
+
+__[Link to the App coming soon!](https://github.com/ccaldarella99/hotdog_not_hotdog)__
+
+<a href="https://www.youtube.com/watch?v=ACmydtFDTGs" target="blank"><img src='./assets/silicon_valley.jpg' /></a>
+
+Inspired by the HBO Series, Silicon Valley, this is a re-creation of Jian-Yang's infamous __*Hotdog/Not-Hotdog*__ App. This app uses a convolutional neural net (CNN) that can run a prediction on whether a picture contains an image of a hotdog or not. 
+
+(App is rendered using Streamlit so a user can upload a picture and have the model run a prediction.)
+
+
+## Data 
+Data is a set of train and test images of classes __Hotdog__ and __Not-Hotdog__ from [this Kaggle competition](https://www.kaggle.com/yashvrdnjain/hotdognothotdog#__sid=js0).
+
+
+<img src='http://www.semantics3.com/blog/content/images/downloaded_images/hot-dog-and-a-not-hot-dog-the-distinction-matters-code-included-8550067fb16/1-VrpXE1hE4rO1roK0laOd7g.png' width="350" />
+<br />
+<strong>Monica:</strong> "That's it? It only does hot dogs?"
+<br />
+<strong>Jian-Yang:</strong> "No, and not hot dog."
+
+
+## About the App
+
+Using Transfer Learning, I was able to create five different Convolutional Neural Nets. THe app allows you to pick which Neural Net you want to use:
+
+ - Xception
+ - MobileNetV2
+ - InceptionV3
+ - ResNet50
+ - VGG16
+
+There is also a Slider bar that allows you to alter the sensitivity of the Model.
+
+<img src="./assets/hnh_demo.png" />
+
+
+## Conclusion
+
+The Xception, MobileNetV2, and InceptionV3 models actually performed very similarly wit haround 90% Accuracy, precision and Recall. The VGG16 model did not do as well, but still pretty well at around 82%. However, the ResNet50 model did not do well at all, unable to break 50% (see table below).
+
+
+| Model       | Validation Accuracy | Validation Precision | Validation Recall |
+| ----------- | ------------------- | -------------------- | ----------------- |
+| Xception    | 0.9363 | 0.9363 | 0.9363 |
+| MobileNetV2 | 0.9068 | 0.9068 | 0.9068 |
+| InceptionV3 | 0.9286 | 0.9286 | 0.9286 |
+| ResNet50    | 0.5000 | 0.5000 | 0.5000 |
+| VGG16       | 0.8261 | 0.8261 | 0.8261 |
+
+
+I would conclude that the Xception model generally performed best, but not much different than the MobileNetV2 and the InceptionV3 Models. Any of these would be a good choice to use with this Application. I would not use the other two since they performed poorly, and both were also quite slow.
+
+
---
+++ hotdog.py
@@ -0,0 +1,115 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import MobileNetV2, VGG16, InceptionV3, Xception, ResNet50
+from PIL import Image
+import os
+
+
+image_1 = 'https://static.wikia.nocookie.net/silicon-valley/images/4/49/Jian_Yang.jpg'
+image_2 = 'http://www.semantics3.com/blog/content/images/downloaded_images/hot-dog-and-a-not-hot-dog-the-distinction-matters-code-included-8550067fb16/1-VrpXE1hE4rO1roK0laOd7g.png'
+image_3 = './assets/silicon_valley.jpg'
+image_4 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRd0abE3OlR-F7Su6Hrv8q_Wf-3J4ZGzY2KzQ&usqp=CAU'
+url_1 = 'https://www.youtube.com/watch?v=ACmydtFDTGs'
+hnh = './assets/hnh.png'
+
+st.title("Hotdog, Not-Hotdog")
+col1, col2 = st.beta_columns(2)
+col3, col4 = st.beta_columns(2)
+
+st.header('')  # for proper spacing with slider, etc.
+pick_model = col2.radio('Pick a Learning-Transfer Model', ("Xception","MobileNetV2", "InceptionV3", "ResNet50", "VGG16"), index=0)
+sensitivity = col2.slider('Sensitivity (0 = all hotdogs, 100 = no hotdogs)', 0, 100, 50)
+uploaded_file = col2.file_uploader("Upload an image...", type="jpg")
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+
+    with st.spinner(text="  \nClassifying..."):
+        # initialize Transfer-Learning Model
+        train_gen_aug = ImageDataGenerator(
+            rescale = 1/255.,
+            shear_range=0.1,
+            zoom_range = 0.2,
+            horizontal_flip = True,
+            vertical_flip=True
+        )
+
+        ### MODEL SET HERE ###
+        if(pick_model == 'Xception'):
+            conv_base = Xception(include_top=False, input_shape=(299, 299, 3))
+            reloaded_model = tf.keras.models.load_model('./models/hotdog_model_1.h5')
+        elif(pick_model == 'MobileNetV2'):
+            conv_base = MobileNetV2(include_top=False, input_shape=(299, 299, 3))
+            reloaded_model = tf.keras.models.load_model('./models/hotdog_model_2.h5')
+        elif(pick_model == 'InceptionV3'):
+            conv_base = InceptionV3(include_top=False, input_shape=(299, 299, 3))
+            reloaded_model = tf.keras.models.load_model('./models/hotdog_model_3.h5')
+        elif(pick_model == 'ResNet50'):
+            conv_base = ResNet50(include_top=False, input_shape=(299, 299, 3))
+            reloaded_model = tf.keras.models.load_model('./models/hotdog_model_4.h5')
+        elif(pick_model == 'VGG16'):
+            conv_base = VGG16(include_top=False, input_shape=(299, 299, 3))
+            reloaded_model = tf.keras.models.load_model('./models/hotdog_model_5.h5')
+
+        img_arr = np.array(image)
+        img_arr = img_arr.reshape(1, 299, 299, 3)
+        img_aug = train_gen_aug.flow(img_arr)
+        p = conv_base.predict(img_aug)
+        preds = reloaded_model.predict(p)
+
+        offset = 0
+        if(sensitivity == 0):
+            offset = 1
+        if(sensitivity == 100):
+            offset = -1
+        adj_sens = 1 - ((sensitivity + offset) / 100)
+
+        if(preds[0][0] <= adj_sens):
+            hnh = './assets/hotdog.png'
+        else:
+            hnh = './assets/nothotdog.png'
+
+        import base64
+        from io import BytesIO
+        st.markdown(
+            """
+            <style>
+            .container {
+                display: flex;
+                position: relative;
+                top: -50px;
+            }
+            .hnh-img {
+                position: relative;
+                top: 100px;
+                z-index:2;
+            }
+            </style>
+            """,
+            unsafe_allow_html=True
+        )
+
+        buffered = BytesIO()
+        image.save(buffered, format="JPEG")
+        image_base64 = base64.b64encode(buffered.getvalue())
+
+        col1.markdown(
+            f"""
+            <div class="container">
+                <div>
+                <img class="hnh-img" src="data:image/png;base64,{base64.b64encode(open(hnh, "rb").read()).decode()}">
+                <div>
+                </div>
+                <img src="data:image/png;base64,{image_base64.decode()}">
+                </div>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
+
+
